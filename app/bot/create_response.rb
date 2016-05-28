633d918362679c17d9e430ab44ae96c7f6eab11c
nikolalsvk <nikolaseap@gmail.com>
2016-05-28 18:22:10 +0200

    Fix initializing

--- app/bot/create_response.rb
+++ app/bot/create_response.rb
@@ -5,15 +5,15 @@ class CreateResponse
   end
 
   def create
-    response = { "content" => "", "attachment" => "" }
+    response = { :content => "", :attachment => "" }
 
     if @text.include? "hi"
-      response["content"] << "Hi there! How can I help you today?"
+      response[:content] << "Hi there! How can I help you today?"
     end
 
     if @text.include? "loan"
-      response["content"] << "In our offer, we have these loans available"
-      response["attachment"] << "attachment"
+      response[:content] << "In our offer, we have these loans available"
+      response[:attachment] << "attachment"
     end
 
     response

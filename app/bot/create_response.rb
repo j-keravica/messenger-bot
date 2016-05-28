fcc3be1c6c1afc3bd8a36e1e219ae075061dbd15
nikolalsvk <nikolaseap@gmail.com>
2016-05-28 18:12:43 +0200

    Fix include

--- app/bot/create_response.rb
+++ app/bot/create_response.rb
@@ -7,11 +7,11 @@ class CreateResponse
   def create
     response = {}
 
-    if @text.includes? "hi"
+    if @text.include? "hi"
       response["content"] << "Hi there! How can I help you today?"
     end
 
-    if @text.includes? "loan"
+    if @text.include? "loan"
       response["content"] << "In our offer, we have these loans available"
       response["attachment"] << "attachment"
     end

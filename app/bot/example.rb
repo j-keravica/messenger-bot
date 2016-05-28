e098bfd8dc3a075c924007373fd5909b9d75c599
nikolalsvk <nikolaseap@gmail.com>
2016-05-29 00:58:40 +0200

    Add postback scenario

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -16,3 +16,30 @@ Bot.on :message do |message|
     message: resp
   )
 end
+
+Bot.on :postback do |postback|
+  postback.sender    # => { 'id' => '1008372609250235' }
+  postback.recipient # => { 'id' => '2015573629214912' }
+  postback.sent_at   # => 2016-04-22 21:30:36 +0200
+  postback.payload   # => 'EXTERMINATE'
+
+  if postback.payload == 'YES'
+    puts "Human #{postback.recipient} said Yes"
+
+    Bot.deliver(
+      recipient: postback.sender,
+      message: {
+        text: "OK, 10 000 RSD sent to Milos. Your main account is now at 24 501 RSD."
+      }
+    )
+  elsif postback.payload == 'NO'
+    puts "Human #{postback.recipient} said No"
+
+    Bot.deliver(
+      recipient: postback.sender,
+      message: {
+        text: "Sure, anything else?"
+      }
+    )
+  end
+end
